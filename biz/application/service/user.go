6f1d30553c22c76235571c7bf371e02de017a306
foliet <1843619244@qq.com>
2023-12-09 18:02:15 +0800

    fix: panic

--- biz/application/service/user.go
+++ biz/application/service/user.go
@@ -10,7 +10,7 @@ import (
 	"github.com/google/wire"
 	"github.com/xh-polaris/gopkg/pagination"
 	"github.com/xh-polaris/gopkg/pagination/esp"
-	"github.com/xh-polaris/service-idl-gen-go/kitex_gen/meowchat/user"
+	genuser "github.com/xh-polaris/service-idl-gen-go/kitex_gen/meowchat/user"
 	"github.com/zeromicro/go-zero/core/jsonx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -21,11 +21,11 @@ import (
 )
 
 type UserService interface {
-	GetUser(ctx context.Context, req *user.GetUserReq) (res *user.GetUserResp, err error)
-	GetUserDetail(ctx context.Context, req *user.GetUserDetailReq) (res *user.GetUserDetailResp, err error)
-	UpdateUser(ctx context.Context, req *user.UpdateUserReq) (res *user.UpdateUserResp, err error)
-	SearchUser(ctx context.Context, req *user.SearchUserReq) (res *user.SearchUserResp, err error)
-	CheckIn(ctx context.Context, req *user.CheckInReq) (res *user.CheckInResp, err error)
+	GetUser(ctx context.Context, req *genuser.GetUserReq) (res *genuser.GetUserResp, err error)
+	GetUserDetail(ctx context.Context, req *genuser.GetUserDetailReq) (res *genuser.GetUserDetailResp, err error)
+	UpdateUser(ctx context.Context, req *genuser.UpdateUserReq) (res *genuser.UpdateUserResp, err error)
+	SearchUser(ctx context.Context, req *genuser.SearchUserReq) (res *genuser.SearchUserResp, err error)
+	CheckIn(ctx context.Context, req *genuser.CheckInReq) (res *genuser.CheckInResp, err error)
 }
 
 type UserServiceImpl struct {
@@ -41,14 +41,14 @@ var UserSet = wire.NewSet(
 	wire.Bind(new(UserService), new(*UserServiceImpl)),
 )
 
-func (s *UserServiceImpl) GetUser(ctx context.Context, req *user.GetUserReq) (res *user.GetUserResp, err error) {
+func (s *UserServiceImpl) GetUser(ctx context.Context, req *genuser.GetUserReq) (res *genuser.GetUserResp, err error) {
 	user1, err := s.UserMongoMapper.FindOne(ctx, req.UserId)
 	if err != nil {
 		return nil, err
 	}
 
-	return &user.GetUserResp{
-		User: &user.UserPreview{
+	return &genuser.GetUserResp{
+		User: &genuser.UserPreview{
 			Id:        user1.ID.Hex(),
 			AvatarUrl: user1.AvatarUrl,
 			Nickname:  user1.Nickname,
@@ -56,38 +56,38 @@ func (s *UserServiceImpl) GetUser(ctx context.Context, req *user.GetUserReq) (re
 	}, nil
 }
 
-func (s *UserServiceImpl) GetUserDetail(ctx context.Context, req *user.GetUserDetailReq) (res *user.GetUserDetailResp, err error) {
-	user1, err := s.UserMongoMapper.FindOne(ctx, req.UserId)
+func (s *UserServiceImpl) GetUserDetail(ctx context.Context, req *genuser.GetUserDetailReq) (res *genuser.GetUserDetailResp, err error) {
+	user, err := s.UserMongoMapper.FindOne(ctx, req.UserId)
 	if err != nil {
 		if err != consts.ErrNotFound {
 			return nil, err
 		}
-		data := &usermapper.User{}
-		data.ID, err = primitive.ObjectIDFromHex(req.GetUserId())
+		user = &usermapper.User{}
+		user.ID, err = primitive.ObjectIDFromHex(req.GetUserId())
 		if err != nil {
 			return nil, err
 		}
-		data.AvatarUrl = "https://static.xhpolaris.com/cat_world.jpg"
-		data.Nickname = "用户_" + req.GetUserId()[:13]
-		data.UpdateAt = time.Now()
-		data.CreateAt = time.Now()
-		err = s.UserMongoMapper.Insert(ctx, data)
+		user.AvatarUrl = "https://static.xhpolaris.com/cat_world.jpg"
+		user.Nickname = "用户_" + req.GetUserId()[:13]
+		user.UpdateAt = time.Now()
+		user.CreateAt = time.Now()
+		err = s.UserMongoMapper.Insert(ctx, user)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	return &user.GetUserDetailResp{
-		User: &user.UserDetail{
-			Id:        user1.ID.Hex(),
-			AvatarUrl: user1.AvatarUrl,
-			Nickname:  user1.Nickname,
-			Motto:     user1.Motto,
+	return &genuser.GetUserDetailResp{
+		User: &genuser.UserDetail{
+			Id:        user.ID.Hex(),
+			AvatarUrl: user.AvatarUrl,
+			Nickname:  user.Nickname,
+			Motto:     user.Motto,
 		},
 	}, nil
 }
 
-func (s *UserServiceImpl) UpdateUser(ctx context.Context, req *user.UpdateUserReq) (res *user.UpdateUserResp, err error) {
+func (s *UserServiceImpl) UpdateUser(ctx context.Context, req *genuser.UpdateUserReq) (res *genuser.UpdateUserResp, err error) {
 	oid, err := primitive.ObjectIDFromHex(req.User.Id)
 	if err != nil {
 		return nil, consts.ErrInvalidObjectId
@@ -109,10 +109,10 @@ func (s *UserServiceImpl) UpdateUser(ctx context.Context, req *user.UpdateUserRe
 	//urls = append(urls, *u)
 	//go s.SendDelayMessage(urls)
 
-	return &user.UpdateUserResp{}, nil
+	return &genuser.UpdateUserResp{}, nil
 }
 
-func (s *UserServiceImpl) SearchUser(ctx context.Context, req *user.SearchUserReq) (res *user.SearchUserResp, err error) {
+func (s *UserServiceImpl) SearchUser(ctx context.Context, req *genuser.SearchUserReq) (res *genuser.SearchUserResp, err error) {
 	popts := &pagination.PaginationOptions{
 		Limit:     req.Limit,
 		Offset:    req.Offset,
@@ -123,60 +123,60 @@ func (s *UserServiceImpl) SearchUser(ctx context.Context, req *user.SearchUserRe
 	if err != nil {
 		return nil, err
 	}
-	resp := make([]*user.UserPreview, 0, *req.Limit)
+	resp := make([]*genuser.UserPreview, 0, *req.Limit)
 	for _, d := range data {
-		m := &user.UserPreview{
+		m := &genuser.UserPreview{
 			Id:        d.ID.Hex(),
 			Nickname:  d.Nickname,
 			AvatarUrl: d.AvatarUrl,
 		}
 		resp = append(resp, m)
 	}
-	res = &user.SearchUserResp{Users: resp, Total: total}
+	res = &genuser.SearchUserResp{Users: resp, Total: total}
 	if popts.LastToken != nil {
 		res.Token = *popts.LastToken
 	}
 	return res, nil
 }
 
-func (s *UserServiceImpl) CheckIn(ctx context.Context, req *user.CheckInReq) (res *user.CheckInResp, err error) {
+func (s *UserServiceImpl) CheckIn(ctx context.Context, req *genuser.CheckInReq) (res *genuser.CheckInResp, err error) {
 
-	res = new(user.CheckInResp)
+	res = new(genuser.CheckInResp)
 
 	t, err := s.Redis.GetCtx(ctx, "checkInTimes"+req.UserId)
 	if err != nil {
-		return &user.CheckInResp{GetFish: false}, nil
+		return &genuser.CheckInResp{GetFish: false}, nil
 	}
 	r, err := s.Redis.GetCtx(ctx, "checkInDates"+req.UserId)
 	if err != nil {
-		return &user.CheckInResp{GetFish: false}, nil
+		return &genuser.CheckInResp{GetFish: false}, nil
 	} else if r == "" {
 		res.GetFish = true
 		res.GetFishTimes = 1
 		err = s.Redis.SetexCtx(ctx, "checkInTimes"+req.UserId, "1", 604800)
 		if err != nil {
-			return &user.CheckInResp{GetFish: false}, nil
+			return &genuser.CheckInResp{GetFish: false}, nil
 		}
 		err = s.Redis.SetexCtx(ctx, "checkInDates"+req.UserId, strconv.FormatInt(time.Now().Unix(), 10), 604800)
 		if err != nil {
-			return &user.CheckInResp{GetFish: false}, nil
+			return &genuser.CheckInResp{GetFish: false}, nil
 		}
 	} else {
 		times, err := strconv.ParseInt(t, 10, 64)
 		if err != nil {
-			return &user.CheckInResp{GetFish: false}, nil
+			return &genuser.CheckInResp{GetFish: false}, nil
 		}
 		date, err := strconv.ParseInt(r, 10, 64)
 		if err != nil {
-			return &user.CheckInResp{GetFish: false}, nil
+			return &genuser.CheckInResp{GetFish: false}, nil
 		}
 		lastTime := time.Unix(date, 0)
 		err = s.Redis.SetexCtx(ctx, "checkInDates"+req.UserId, strconv.FormatInt(time.Now().Unix(), 10), 604800)
 		if err != nil {
-			return &user.CheckInResp{GetFish: false}, nil
+			return &genuser.CheckInResp{GetFish: false}, nil
 		}
 		if lastTime.Day() == time.Now().Day() && lastTime.Month() == time.Now().Month() && lastTime.Year() == time.Now().Year() {
-			return &user.CheckInResp{GetFish: false}, nil
+			return &genuser.CheckInResp{GetFish: false}, nil
 		}
 		lastYear, lastWeek := lastTime.ISOWeek()
 		nowYear, nowWeek := time.Now().ISOWeek()
@@ -184,13 +184,13 @@ func (s *UserServiceImpl) CheckIn(ctx context.Context, req *user.CheckInReq) (re
 			res.GetFishTimes = times + 1
 			err = s.Redis.SetexCtx(ctx, "checkInTimes"+req.UserId, strconv.FormatInt(times+1, 10), 604800)
 			if err != nil {
-				return &user.CheckInResp{GetFish: false}, nil
+				return &genuser.CheckInResp{GetFish: false}, nil
 			}
 			res.GetFish = true
 		} else {
 			err = s.Redis.SetexCtx(ctx, "checkInTimes"+req.UserId, "1", 604800)
 			if err != nil {
-				return &user.CheckInResp{GetFish: false}, nil
+				return &genuser.CheckInResp{GetFish: false}, nil
 			}
 			res.GetFish = true
 			res.GetFishTimes = 1
